4e27b654d9dc5aa3d1305bacbc7c40fe430212fe
Noxious <133783031+Noxious168@users.noreply.github.com>
2023-05-30 18:56:18 +0630

    30/5/2023
    update

---
+++ spiderman/spiderman.js
@@ -0,0 +1,8 @@
+function left_scroll(){
+	var left=document.querySelector(".scroll-images");
+	left.scrollBy(-350,0);
+}
+function right_scroll(){
+	var right=document.querySelector(".scroll-images");
+	right.scrollBy(350,0);
+}
