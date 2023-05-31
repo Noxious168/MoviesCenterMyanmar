4d9a8352f36b6079825b5dac79ad5ec60a067d94
Noxious <133783031+Noxious168@users.noreply.github.com>
2023-05-31 16:25:56 +0630

    3105

--- spiderman/spiderman.js
+++ spiderman/spiderman.js
@@ -6,3 +6,9 @@ function right_scroll(){
 	var right=document.querySelector(".scroll-images");
 	right.scrollBy(350,0);
 }
+function changeImage(anything){
+	var background=document.getElementById('changebackground');
+	background.style.backgroundImage=anything;
+	background.style.backgroundSize="100% 100vh";
+	background.style.backdropFilter="blur(5px)";
+}
