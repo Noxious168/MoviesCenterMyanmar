88d29ca020d479c59a9302917e3e60143d304401
Noxious <133783031+Noxious168@users.noreply.github.com>
2023-06-26 17:28:17 +0630

    2303202317hr
    update

---
+++ series/series.js
@@ -0,0 +1,17 @@
+const theAvenger=document.getElementById('theAvenger');
+const moonKnight=document.getElementById('moonknight');
+const loki=document.getElementById('loki');
+const sheHulk=document.getElementById('shehulk');
+
+theAvenger.addEventListener("click",()=>{
+	window.location.href="theavenger/avenger.html"
+});
+moonKnight.addEventListener("click",()=>{
+	window.location.href="moonknight/moonknight.html"
+});
+loki.addEventListener("click",()=>{
+	window.location.href="loki/loki.html"
+});
+sheHulk.addEventListener("click",()=>{
+	window.location.href="shehulk/shehulk.html"
+});
